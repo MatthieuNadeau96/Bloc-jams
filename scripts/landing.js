1912db4bb2c1b0f147cdf8f26d3ddd8a1b013cc7
Matthieu <matthieunadeau96@gmail.com>
2017-02-25 13:10:23 -0500

    13
    Submission for checkpoint #13 for bloc jams

--- scripts/landing.js
+++ scripts/landing.js
@@ -26,15 +26,3 @@ var animatePoints = function(points) {
         
       });  
 };
-/*
- window.onload = function() {
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-     window.addEventListener("scroll", function(event) {
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
-         }
-     });
- }
- */
